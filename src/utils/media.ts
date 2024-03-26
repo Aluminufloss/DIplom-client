f8a871d14d1012c0022c26b48fe68477be82b9fb
Vladimir <vladimir.paliy.work@gmail.com>
2024-03-26 21:23:06 +0300

    Add media utils (#1)

---
+++ src/utils/media.ts
@@ -0,0 +1,24 @@
+export function getMediaQuery(minWidth: number): string {
+  return `@media (min-width: ${minWidth}px)`;
+}
+
+export const mediaValues = {
+  desktop: 720,
+  extraDesktop: 940,
+  ld: 1090,     // large desktop
+  extraLd: 1435,
+}
+
+const media = {
+  custom: getMediaQuery,
+  desktop: getMediaQuery(mediaValues.desktop),
+  extraDesktop: getMediaQuery(mediaValues.extraDesktop),
+  ld: getMediaQuery(mediaValues.ld),
+  extraLd: getMediaQuery(mediaValues.extraLd),
+}
+
+export default media;
+
+export function getIsMobile(): boolean {
+  return !window.matchMedia("(min-width: 720px)").matches
+}
