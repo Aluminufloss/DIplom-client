75bef08489448bc771ddc5c10c4d4d9f80bfaa9b
Vladimir <vladimir.paliy.work@gmail.com>
2024-04-01 23:09:22 +0300

    Add theme provider (#4)

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { ThemeProvider } from "styled-components";
+import { createTheme } from "../theme";
+
+export default function ThemeClientProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const theme = createTheme();
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+}
