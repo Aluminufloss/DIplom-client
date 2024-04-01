1b0cfc63349097bbb1a718820e96ce1ad8695f7c
Vladimir <vladimir.paliy.work@gmail.com>
2024-04-01 23:08:33 +0300

    Add forms layout (#4)

---
+++ src/app/(actions)/layout.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { STATIC_URLS } from "@/utils/constant";
+import Image from "next/image";
+import styled from "styled-components";
+
+export default function FormsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <StyledFormsLayout>
+      <div className="image__container">
+        <Image
+          src={`${STATIC_URLS.BACKGROUND}/actions_background.png`}
+          alt="Actions background"
+          fill
+        />
+      </div>
+      <div className="content">{children}</div>
+    </StyledFormsLayout>
+  );
+}
+
+const StyledFormsLayout = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+
+  background-color: ${(props) => props.theme.colorValues.white};
+
+  .image__container {
+    position: relative;
+
+    width: 66%;
+    height: 100%;
+  }
+
+  .content {
+		flex-grow: 1;
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		padding: 40px 24px;
+  }
+`;
