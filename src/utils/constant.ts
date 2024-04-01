b9e02fb1c9a2a17c92447e2c1015206444fc73a5
Vladimir <vladimir.paliy.work@gmail.com>
2024-04-01 23:09:31 +0300

    Add constants file (#4)

---
+++ src/utils/constant.ts
@@ -0,0 +1,13 @@
+export const STATIC_BASE_URL = ``;
+
+export const STATIC_URLS = {
+  IMAGES: `${STATIC_BASE_URL}/images`,
+  BACKGROUND: `${STATIC_BASE_URL}/images/background`,
+  LOGO: `${STATIC_BASE_URL}/images/logos`,
+}
+
+export enum DeviceTypes {
+  mobile = 'mobile',
+  desktop = 'desktop',
+  tablet = 'tablet',
+}
