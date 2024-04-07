8cee8edd67b3da10d23cd6c578b4b1188c65530b
Vladimir <vladimir.paliy.work@gmail.com>
2024-04-07 19:08:53 +0300

    Add layout for change password form (#4)

---
+++ src/app/changePassword/layout.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import styled from "styled-components";
+
+import media from "@/utils/media";
+
+export default function FormsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <StyledFormsLayout>
+      <div className="content">{children}</div>
+    </StyledFormsLayout>
+  );
+}
+
+const StyledFormsLayout = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+
+  background-color: ${(props) => props.theme.colorValues.white};
+
+  .content {
+    flex-grow: 1;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 40px 24px;
+  }
+
+  ${media.custom(1150)} {
+    justify-content: center;
+    align-items: center;
+  }
+
+  ${media.desktop} {
+    padding: 28px 24px;
+
+    .content {
+      padding: 0;
+    }
+  }
+`;
