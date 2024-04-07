37859cffb8a3c9d658ab9c8c956252ed4251bc75
Vladimir <vladimir.paliy.work@gmail.com>
2024-04-07 19:12:00 +0300

    Add action item (#5)

---
+++ src/components/UI/actionItem/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import ReusableImage from "../image";
+
+type PropsType = {
+  onClick: () => void;
+  imageSrc: string;
+  imageAlt: string;
+  clasName?: string;
+};
+
+const ActionItem: React.FC<PropsType> = (props) => {
+  return (
+    <StyledActionItem onClick={props.onClick} className={props.clasName}>
+      <ReusableImage src={props.imageSrc} alt={props.imageAlt} />
+    </StyledActionItem>
+  );
+};
+
+const StyledActionItem = styled.button`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	width: 24px;
+	height: 24px;
+
+	transition: all .3s ease;
+
+	&:hover {
+		transform: scale(1.1);
+	}
+`;
+
+export default ActionItem;
