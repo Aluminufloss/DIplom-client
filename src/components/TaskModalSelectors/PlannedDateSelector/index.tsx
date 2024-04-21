6c16f9909122c5810503503e9302f7ffa033fb92
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-21 16:19:05 +0300

    Add planned date selector(#10)

--- src/components/TaskModalSelectors/PlannedDateSelector/index.tsx
+++ src/components/TaskModalSelectors/PlannedDateSelector/index.tsx
@@ -1,9 +1,52 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
-const PlannedDateSelector: React.FC = () => {
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
+
+type ParamsType = {
+  className?: string;
+};
+
+const PlannedDateSelector: React.FC<ParamsType> = (props) => {
+  new AirDatepicker('timepicker', {
+    selectedDates: [new Date()],
+    timepicker: true,
+  });
+
+
   return (
-    <div>CategorySelector</div>
-  )
-}
+    <StyledPlannedDateSelector className={props.className}>
+      <span className="selector__title">Запланировать</span>
+      <div id="timepicker" className="selector__timepicker"/>
+    </StyledPlannedDateSelector>
+  );
+};
+
+const StyledPlannedDateSelector = styled.div`
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  .selector {
+    &__title {
+      ${(props) => props.theme.typography.fnMedium};
+      ${(props) => props.theme.typography.fnTitle2};
+      color: ${(props) => props.theme.colorValues.black};
+    }
+
+    &__timepicker {
+      width: 100%;
+      max-width: 180px;
+      height: 36px;
+
+      border: 1px solid ${(props) => props.theme.colorValues.lightGrey};
+      border-radius: 5px;
+
+      margin-left: 12px;
+    }
+  }
+`;
 
 export default PlannedDateSelector;
