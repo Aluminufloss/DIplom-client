3267c3233a667e1136b86e7fb6f3c7bea3dd05f9
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-21 22:30:39 +0300

    Add reducers for task priority  and handling repeat days (#10)

--- src/store/slices/TaskModal/index.ts
+++ src/store/slices/TaskModal/index.ts
@@ -1,20 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { taskModalInitState } from "./initialState";
+import { SelectesdDayType } from "./models";
 
 export const taskModal = createSlice({
   name: "taskModal",
   initialState: taskModalInitState,
   reducers: {
-    setModalVisibility: (state) => {
-      state.isModalVisible = !state.isModalVisible;
+    setModalVisibility: (state, action) => {
+      state.isModalVisible = action.payload;
     },
 
     setModalParams: (state, action) => {
       state.modalParams = action.payload;
     },
+
+    setTaskPriority: (state, action) => {
+      state.modalParams.priority = action.payload;
+    },
+
+    setRepeatDay: (state, action: PayloadAction<SelectesdDayType>) => {
+      state.modalParams.repeatDays.days = state.modalParams.repeatDays.days.map(
+        (item) => {
+          return item.day === action.payload.day
+            ? { ...item, isSelected: !item.isSelected }
+            : item;
+        }
+      );
+    },
   },
 });
 
-export const { setModalVisibility } = taskModal.actions;
+export const {
+  setModalVisibility,
+  setModalParams,
+  setTaskPriority,
+  setRepeatDay,
+} = taskModal.actions;
 
 export default taskModal.reducer;
