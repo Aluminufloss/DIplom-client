e70f242de96172c637714c6adfe0bacce3491715
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-21 22:29:40 +0300

    Add modal overlay (#10)

--- src/components/Modals/TaskModal.tsx
+++ src/components/Modals/TaskModal.tsx
@@ -11,7 +11,7 @@ import PlannedDateSelector from "../TaskModalSelectors/PlannedDateSelector";
 
 type ParamsType = {};
 
-const TaskModal: React.FC<ParamsType> = (props) => {
+const TaskModal: React.FC<ParamsType> = () => {
   const modalInfo = useAppSelector((state) => state.taskModal);
 
   return (
@@ -33,32 +33,44 @@ const TaskModal: React.FC<ParamsType> = (props) => {
                 name="taskDescription"
                 className="modal__textarea"
               />
-              <PrioritySelector className="modal__priority" />
+              <PrioritySelector
+                className="modal__priority"
+                currentPriority={modalInfo.modalParams.priority}
+              />
               <RepeatSelector
                 className="modal__repeat"
                 selectedDays={modalInfo.modalParams.repeatDays.days}
               />
-              <PlannedDateSelector className="modal__date" />
+              <PlannedDateSelector />
             </div>
           </StyledModal>
         )}
       </Formik>
+      <StyledOverlay $isModalVisible={modalInfo.isModalVisible}/>
     </>
   );
 };
 
 const StyledModal = styled(Form)<{ $isModalVisible: boolean }>`
   position: absolute;
-  bottom: ${(props) => (props.$isModalVisible ? "0" : "0")};
+  top: ${(props) => (props.$isModalVisible ? "0" : "-200%")};
   right: 50%;
   transform: translateX(50%);
+
+  width: 100%;
+  max-width: 840px;
+  max-height: 80vh;
 
   z-index: 300;
 
   transition: all 0.5s ease;
 
-  width: 100%;
-  max-width: 720px;
+  overflow-y: scroll;
+  scrollbar-width: thin;
+
+  div::-webkit-scrollbar {
+    display: none;
+  }
 
   background-color: white;
 
@@ -85,17 +97,45 @@ const StyledModal = styled(Form)<{ $isModalVisible: boolean }>`
       height: 120px;
 
       resize: none;
+      overflow: scroll;
+
+      &::-webkit-scrollbar {
+        width: 0;
+      }
 
       padding: 12px 16px;
       margin-bottom: 20px;
 
       border: 1px solid ${(props) => props.theme.colorValues.lightGrey};
+      border-radius: 5px;
 
       &::placeholder {
         color: ${(props) => props.theme.colorValues.darkGrey};
       }
     }
   }
+`;
+
+const StyledOverlay = styled.div<{ $isModalVisible: boolean }>`
+  position: "absolute";
+
+  display: ${props => !props.$isModalVisible && "none"};
+
+  top: -77px;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  z-index: 200;
+
+  background-color: rgba(0, 0, 0, 0.5);
+
+  transition: opacity 0.5s ease;
+
+  overflow: hidden;
+
+  opacity: ${(props) => (props.$isModalVisible ? 1 : 0)};
 `;
 
 export default TaskModal;
