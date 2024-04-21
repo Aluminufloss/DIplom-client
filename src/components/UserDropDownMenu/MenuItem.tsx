aca78109bc9349f319d018475cd1b7e2a9f0fe0f
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-21 19:15:59 +0300

    Update user dropdown menu logic (#9)

--- src/components/UserDropDownMenu/MenuItem.tsx
+++ src/components/UserDropDownMenu/MenuItem.tsx
@@ -6,7 +6,7 @@ import { STATIC_URLS } from "@/utils/constant";
 import ReusableImage from "@/components/UI/image";
 
 type PropsType = {
-  type: "password" | "themeDark" | "themeLight" | "username";
+  type: "password" | "themeDark" | "themeLight" | "username" | "logout";
   title: string;
   onClick: () => void;
   className?: string;
@@ -17,13 +17,14 @@ const ImagePaths = {
   themeDark: `${STATIC_URLS.SVG_ICONS}/sun.svg`,
   themeLight: `${STATIC_URLS.SVG_ICONS}/sun.svg`,
   username: `${STATIC_URLS.SVG_ICONS}/edit.svg`,
+  logout: `${STATIC_URLS.SVG_ICONS}/logout.svg`,
 };
 
 const MenuItem: React.FC<PropsType> = (props) => {
   const imagePath = ImagePaths[props.type];
 
   return (
-    <StyledMenuItem className={props.className}>
+    <StyledMenuItem className={props.className} onClick={props.onClick}>
       <ReusableImage
         src={imagePath}
         alt="Dropdown menu item"
@@ -44,6 +45,8 @@ const StyledMenuItem = styled.li`
   padding: 10px 12px;
 
   transition: background-color 0.2s linear;
+
+  cursor: pointer;
 
   &:hover {
     background-color: ${(props) => props.theme.colorValues.lightGrey};
