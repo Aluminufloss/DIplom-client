5078e50ce0eeb58f6ef3f2d12034a716079cf58f
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-21 16:18:03 +0300

    Add task modal to tasks layout (#10)

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -4,36 +4,25 @@ import React from "react";
 import { Provider } from "react-redux";
 import styled from "styled-components";
 
-import { UserResponseType } from "@/models";
 import { store } from "@/store";
-import { typedFetchWrapper } from "@/utils/typedFetchWrapper";
 
 import Header from "../../components/Header";
 import TabbedSidebar from "@/components/SidebarModals/TabbedSidebar";
-import TaskSidebar from "@/components/SidebarModals/TaskSidebar";
 import UserSidebar from "@/components/SidebarModals/UserSidebar";
+import TaskModal from "@/components/Modals/TaskModal";
 
 const TasksLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  React.useEffect(() => {
-    (async () => {
-      const response = await typedFetchWrapper<UserResponseType>({ url: "/tasks/api" });
-
-      localStorage.setItem("accessToken", response.accessToken);
-    })();
-  }, []);
-
-
   return (
     <Provider store={store}>
       <StyledLayout>
         <Header />
         <TabbedSidebar />
-        <TaskSidebar />
         <UserSidebar />
+        <TaskModal />
         <div className={"content"}>{children}</div>
       </StyledLayout>
     </Provider>
