3e9578306b9d047fbad95bae11be1521fb9219b0
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-21 22:30:00 +0300

    Update date selector (#10)

--- src/components/TaskModalSelectors/PlannedDateSelector/index.tsx
+++ src/components/TaskModalSelectors/PlannedDateSelector/index.tsx
@@ -1,24 +1,22 @@
+"use client"
+
 import React from "react";
 import styled from "styled-components";
-
-import AirDatepicker from 'air-datepicker';
-import 'air-datepicker/air-datepicker.css';
 
 type ParamsType = {
   className?: string;
 };
 
 const PlannedDateSelector: React.FC<ParamsType> = (props) => {
-  new AirDatepicker('timepicker', {
-    selectedDates: [new Date()],
-    timepicker: true,
-  });
-
-
   return (
     <StyledPlannedDateSelector className={props.className}>
       <span className="selector__title">Запланировать</span>
-      <div id="timepicker" className="selector__timepicker"/>
+      <input
+        type="text"
+        placeholder="Выберите дату"
+        readOnly
+        className="selector__timepicker"
+      />
     </StyledPlannedDateSelector>
   );
 };
@@ -28,23 +26,31 @@ const StyledPlannedDateSelector = styled.div`
 
   display: flex;
   align-items: center;
+  justify-content: space-between;
 
   .selector {
     &__title {
+      color: ${(props) => props.theme.colorValues.darkGrey};
+      ${(props) => props.theme.typography.fnTitle1};
       ${(props) => props.theme.typography.fnMedium};
-      ${(props) => props.theme.typography.fnTitle2};
-      color: ${(props) => props.theme.colorValues.black};
     }
 
     &__timepicker {
       width: 100%;
-      max-width: 180px;
+      max-width: 240px;
       height: 36px;
 
       border: 1px solid ${(props) => props.theme.colorValues.lightGrey};
       border-radius: 5px;
 
       margin-left: 12px;
+      padding: 0 12px;
+
+      &::placeholder {
+        color: ${(props) => props.theme.colorValues.darkGrey};
+        ${(props) => props.theme.typography.fnLabel2};
+        ${(props) => props.theme.typography.fnMedium};
+      }
     }
   }
 `;
