0e5cc2d16ef475ac0a10f2ee20f6b7ac65ae999c
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-26 15:36:00 +0300

    Add ITask interface (#15)

---
+++ src/api/models/Response/Tasks/ITask.ts
@@ -0,0 +1,13 @@
+import { TaskPriorityType, TaskStatusType } from "@/models";
+
+export interface ITask {
+  taskId: string;
+  listId: string;
+  title: string;
+  status: TaskStatusType;
+  priority: TaskPriorityType;
+  plannedDate: Date;
+  repeatDays?: number[];
+  description?: string;
+  category?: string;
+}
