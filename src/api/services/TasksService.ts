7b08b924e2bee6f493dc645f52d3e2ef64869d6a
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-26 15:36:23 +0300

    Add TasksService (#15)

---
+++ src/api/services/TasksService.ts
@@ -0,0 +1,43 @@
+import { AxiosResponse } from "axios";
+import $api from "@/axios";
+
+import { TasksResponse } from "../models/Response/Tasks";
+import { ITask } from "../models/Response/Tasks/ITask";
+
+export default class TasksTodayService {
+  public static async getTodayTasks(
+    userId: string
+  ): Promise<AxiosResponse<TasksResponse>> {
+    return await $api.post<TasksResponse>(`/tasks/today`, {
+      userId,
+    });
+  }
+
+  public static async getTask(
+    taskId: string
+  ): Promise<AxiosResponse<TasksResponse>> {
+    return await $api.post<TasksResponse>(`/get/task`, {
+      taskId,
+    });
+  }
+
+  public static async updateTask(
+    updatedTaskData: ITask
+  ): Promise<AxiosResponse<TasksResponse>> {
+    return await $api.patch<TasksResponse>("/update/task", { updatedTaskData });
+  }
+
+  public static async deleteTask(
+    taskId: string
+  ): Promise<void> {
+    await $api.post<TasksResponse>(`/delete/task`, {
+      taskId,
+    });
+  }
+
+  public static async createTask(
+    taskData: ITask
+  ): Promise<AxiosResponse<TasksResponse>> {
+    return await $api.post<TasksResponse>("/create/task", { taskData });
+  }
+}
