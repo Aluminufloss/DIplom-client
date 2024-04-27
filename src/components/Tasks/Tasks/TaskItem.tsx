54f88039ab1f72e2a9b6bda0c4d663ace45162c1
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-27 18:53:58 +0300

    Update task item logic (#14)

--- src/components/Tasks/Tasks/TaskItem.tsx
+++ src/components/Tasks/Tasks/TaskItem.tsx
@@ -1,18 +1,67 @@
+"use client";
+
 import React from "react";
 import styled from "styled-components";
+import { format } from "date-fns/format";
 
-import { TaskType } from "@/models";
+
+import { isDatesEqual } from "@/utils/dateUtils";
+import { useAppDispatch } from "@/utils/hooks/useAppDispatch";
+import { ITask } from "@/api/models/Response/Tasks/ITask";
+import { changeTaskStatus } from "@/store/slices/Tasks/thunks";
 
 type PropsType = {
-  item?: TaskType;
+  task: ITask;
 };
 
 export const TaskItem: React.FC<PropsType> = (props) => {
+  const dispatch = useAppDispatch();
+
+  const handleChangeTaskStatus = React.useCallback(() => {
+    const changedTaskStatus =
+      props.task.status === "active" ? "completed" : "active";
+    dispatch(
+      changeTaskStatus({ taskId: props.task.taskId, status: changedTaskStatus })
+    );
+  }, [props.task.taskId, props.task.status]);
+
+  const handleOpenTaskModal = React.useCallback(() => {
+    // const modalParams: ModalParamsType = {
+    //   modalType: "edit",
+    //   taskName: props.task.title,
+    //   taskDescription: props.task.description,
+    //   priority: props.task.priority,
+    //   plannedDate: props.task.plannedDate,
+    //   repeatDays: props.task.repeatDays,
+    //   selectedList: SelectedListType;
+    //   selectedCategory?: SelectedCategoryType;
+    // }
+    // dispatch(setModalParams());
+  }, []);
+
+  const isToday = isDatesEqual(props.task.plannedDate, new Date());
+
   return (
-    <StyledTaskItem>
-      <input type="checkbox" className="task__checkbox" />
-      <p className="task__text">Tast input text test</p>
-    </StyledTaskItem>
+    <>
+      <StyledTaskItem onClick={handleOpenTaskModal} id="datepicker">
+        <input
+          onClick={handleChangeTaskStatus}
+          type="checkbox"
+          className="task__checkbox"
+          checked={props.task.status === "completed"}
+          readOnly
+        />
+        <p className="task__text">{props.task.title}</p>
+        {!isToday && (
+          <>
+            <span className="task__separator">|</span>
+            <p className="task__date">
+              {format(props.task.plannedDate, "dd.MM.yyyy")}
+            </p>
+          </>
+        )}
+      </StyledTaskItem>
+    </>
   );
 };
 
@@ -31,14 +80,14 @@ const StyledTaskItem = styled.div`
   border: 1px solid ${(props) => props.theme.colorValues.lightGrey};
   border-radius: 5px;
 
-	transition: background-color .3s ease;
+  transition: background-color 0.3s ease;
 
-	&:hover {
-		background-color: ${(props) => props.theme.colorValues.strokeGrey};
-	}
+  &:hover {
+    background-color: ${(props) => props.theme.colorValues.strokeGrey};
+  }
 
   input[type="checkbox"] {
-		position: relative;
+    position: relative;
 
     appearance: none;
 
@@ -53,16 +102,16 @@ const StyledTaskItem = styled.div`
   input[type="checkbox"]::before {
     content: "";
 
-		position: absolute;
-		top: 0;
-		left: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
 
     width: 100%;
     height: 100%;
 
-    background-color:${props => props.theme.colorValues.primary};
-		
-		transition: all .1s ease;
+    background-color: ${(props) => props.theme.colorValues.primary};
+
+    transition: all 0.1s ease;
     transform: scale(0);
   }
 
@@ -87,6 +136,18 @@ const StyledTaskItem = styled.div`
       color: ${(props) => props.theme.colorValues.black};
 
       margin-left: 16px;
+    }
+
+    &__separator {
+      color: ${(props) => props.theme.colorValues.darkGrey};
+
+      margin: 0 5px;
+    }
+
+    &__date {
+      ${(props) => props.theme.typography.fnTitle2}
+      ${(props) => props.theme.typography.fnMedium};
+      color: ${(props) => props.theme.colorValues.grey};
     }
   }
 `;
