10617335dc28ec9653a198228ca9ca30c320dcf0
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-04-27 18:50:53 +0300

    Update task services (#14)

--- src/api/services/TasksService.ts
+++ src/api/services/TasksService.ts
@@ -1,43 +1,54 @@
 import { AxiosResponse } from "axios";
 import $api from "@/axios";
 
-import { TasksResponse } from "../models/Response/Tasks";
+import { TaskStatusType } from "@/models";
+import { TasksResponseType } from "../models/Response/Tasks";
 import { ITask } from "../models/Response/Tasks/ITask";
 
 export default class TasksService {
   public static async getTodayTasks(
     userId: string
-  ): Promise<AxiosResponse<TasksResponse>> {
-    return await $api.post<TasksResponse>(`/tasks/today`, {
+  ): Promise<AxiosResponse<TasksResponseType>> {
+    return await $api.post<TasksResponseType>(`/tasks/today`, {
       userId,
     });
   }
 
   public static async getTask(
     taskId: string
-  ): Promise<AxiosResponse<TasksResponse>> {
-    return await $api.post<TasksResponse>(`/get/task`, {
+  ): Promise<AxiosResponse<TasksResponseType>> {
+    return await $api.post<TasksResponseType>(`/get/task`, {
       taskId,
     });
   }
 
   public static async updateTask(
     updatedTaskData: ITask
-  ): Promise<AxiosResponse<TasksResponse>> {
-    return await $api.patch<TasksResponse>("/update/task", { updatedTaskData });
+  ): Promise<AxiosResponse<TasksResponseType>> {
+    return await $api.patch<TasksResponseType>("/update/task", { updatedTaskData });
   }
 
   public static async deleteTask(
     taskId: string
   ): Promise<void> {
-    await $api.post<TasksResponse>(`/delete/task`, {
+    await $api.post<TasksResponseType>(`/delete/task`, {
       taskId,
     });
   }
 
   public static async createTask(
     taskData: ITask
-  ): Promise<AxiosResponse<TasksResponse>> {
-    return await $api.post<TasksResponse>("/create/task", { taskData });
+  ): Promise<AxiosResponse<TasksResponseType>> {
+    return await $api.post<TasksResponseType>("/create/task", { taskData });
+  }
+
+  public static async changeTaskStatus(
+    taskId: string,
+    status: TaskStatusType
+  ): Promise<AxiosResponse<TasksResponseType>> {
+    return await $api.post<TasksResponseType>(`/changeStatus/task`, {
+      taskId,
+      status,
+    });
   }
 }
