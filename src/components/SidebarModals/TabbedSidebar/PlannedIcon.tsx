534d280e5119a061d5b214ab56802415e9620507
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-02 22:56:37 +0300

    Add new planned icon component (#12)

--- src/components/SidebarModals/TabbedSidebar/PlannedIcon.tsx
+++ src/components/SidebarModals/TabbedSidebar/PlannedIcon.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 type PropsType = {
-  fill?: string;
+  color?: string;
 };
 
 const PlannedIcon: React.FC<PropsType> = (props) => {
@@ -15,43 +15,43 @@ const PlannedIcon: React.FC<PropsType> = (props) => {
     >
       <path
         d="M19 4H5C3.89543 4 3 4.89543 3 6V20C3 21.1046 3.89543 22 5 22H19C20.1046 22 21 21.1046 21 20V6C21 4.89543 20.1046 4 19 4Z"
-        stroke="black"
+        stroke={props.color ?? "black"}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M16 2V6"
-        stroke="black"
+        stroke={props.color ?? "black"}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M8 2V6"
-        stroke="black"
+        stroke={props.color ?? "black"}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M3 10H21"
-        stroke="black"
+        stroke={props.color ?? "black"}
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
-      <rect x="5" y="12" width="2" height="2" fill="#171717" />
-      <rect x="5" y="15" width="2" height="2" fill="#171717" />
-      <rect x="5" y="18" width="2" height="2" fill="#171717" />
-      <rect x="9" y="18" width="2" height="2" fill="#171717" />
-      <rect x="9" y="15" width="2" height="2" fill="#171717" />
-      <rect x="13" y="15" width="2" height="2" fill="#171717" />
-      <rect x="13" y="18" width="2" height="2" fill="#171717" />
-      <rect x="17" y="15" width="2" height="2" fill="#171717" />
-      <rect x="9" y="12" width="2" height="2" fill="#171717" />
-      <rect x="13" y="12" width="2" height="2" fill="#171717" />
-      <rect x="17" y="12" width="2" height="2" fill="#171717" />
+      <rect x="5" y="12" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="5" y="15" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="5" y="18" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="9" y="18" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="9" y="15" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="13" y="15" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="13" y="18" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="17" y="15" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="9" y="12" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="13" y="12" width="2" height="2" fill={props.color ?? "#171717"} />
+      <rect x="17" y="12" width="2" height="2" fill={props.color ?? "#171717"} />
     </svg>
   );
 };
