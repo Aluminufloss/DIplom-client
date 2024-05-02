88de49c2b1e403e6de608666583f08f20c9f899f
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-02 22:32:49 +0300

    Update task model (#15)

--- src/api/models/Response/Tasks/ITask.ts
+++ src/api/models/Response/Tasks/ITask.ts
@@ -1,13 +1,14 @@
 import { TaskPriorityType, TaskStatusType } from "@/models";
+import { SelectesdDayType } from "@/store/slices/TaskModal/models";
 
 export interface ITask {
-  taskId: string;
-  listId: string;
+  taskId?: string;
+  listId?: string;
   title: string;
   status: TaskStatusType;
   priority: TaskPriorityType;
   plannedDate: Date;
-  repeatDays: number[];
+  repeatDays: SelectesdDayType[];
   description?: string;
   category?: string;
 }
