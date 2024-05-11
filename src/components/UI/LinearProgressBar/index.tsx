49af040596b7a086bd9000705246e4bf47dd6184
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-11 15:57:12 +0300

    Add linear progress bar (#19)

---
+++ src/components/UI/LinearProgressBar/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import styled from "styled-components";
+
+import LinearProgress, {
+  LinearProgressProps,
+} from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+const LinearProgressBar: React.FC<LinearProgressProps & { value: number }> = (
+  props
+) => {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+const StyledLinearProgressBar = styled.div``;
+
+export default LinearProgressBar;
