a9f71e26ce7a7aed4edd78d5a19efa75cb184216
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-12 18:49:42 +0300

    Add test middleware for all tasks section (#10)

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { cookies } from 'next/headers'; 
+import { NextResponse } from 'next/server' 
+
+import { UserResponseType } from './models'; 
+import { serverSideFetch } from './utils/serverSideFetch'; 
+import { CLIENT_URL } from './utils/constant'; 
+  
+export async function middleware() { 
+  const accessToken = cookies().get('accessToken')?.value; 
+  const refreshToken = cookies().get('refreshToken')?.value; 
+ 
+  const userResponse = await serverSideFetch<UserResponseType>({ 
+    url: "http://localhost:5000/me", 
+    method: "POST", 
+    credentials: "include", 
+    headers: { 
+      "Cookie": `accessToken=${accessToken}; refreshToken=${refreshToken}`
+    } 
+  }); 
+ 
+  if (userResponse?.status === 401) { 
+    return NextResponse.redirect(`${CLIENT_URL}/login`); 
+  } 
+
+  const response = NextResponse.next(); 
+  
+  return response; 
+} 
+ 
+export const config = { 
+  matcher: '/tasks/:path*', 
+}
