59bd8922868e072f1437236b45c34f15e650bbc5
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-14 19:34:14 +0300

    Update tabbed sidebar chose logic of tabs (#10)

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,31 +2,31 @@ import { SelectesdDayType } from "@/store/slices/TaskModal/models";
 
 export const STATIC_BASE_URL = ``;
 
-export const API_URL = 'http://localhost:5000';
-export const CLIENT_URL = 'http://localhost:3000';
+export const API_URL = "http://localhost:5000";
+export const CLIENT_URL = "http://localhost:3000";
 
 export const STATIC_URLS = {
   IMAGES: `${STATIC_BASE_URL}/images`,
   SVG_ICONS: `${STATIC_BASE_URL}/images/svg`,
   BACKGROUND: `${STATIC_BASE_URL}/images/background`,
   LOGO: `${STATIC_BASE_URL}/images/logos`,
-}
+};
 
 export enum DeviceTypes {
-  mobile = 'mobile',
-  desktop = 'desktop',
-  tablet = 'tablet',
+  mobile = "mobile",
+  desktop = "desktop",
+  tablet = "tablet",
 }
 
 export enum AppRoutes {
-  login = 'login',
-  registration = 'registration',
-  tasksToday = 'tasks/today',
-  tasksAll = 'tasks/all',
-  tasksPlanned = 'tasks/planned',
-  tasksAnalytics = 'tasks/analytics',
-  sendChangePasswordLink = 'changePassword',
-  list = "tasks/list"
+  login = "login",
+  registration = "registration",
+  tasksToday = "tasks/today",
+  tasksAll = "tasks/all",
+  tasksPlanned = "tasks/planned",
+  tasksAnalytics = "tasks/analytics",
+  sendChangePasswordLink = "changePassword",
+  list = "tasks/list",
 }
 
 export enum AppPaths {
