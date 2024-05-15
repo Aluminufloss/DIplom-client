1a788dccdf1b2b8dc1a67454652ac8621e4840b9
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-15 14:23:32 +0300

    Update analytics section data fetching logic (#19)

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,21 +1,52 @@
-"use client"
+"use client";
 
+import React from "react";
 import styled from "styled-components";
-import AnalyticsSectionBar from "./AnalyticsSectionBar";
+
+import { AnalyticsType } from "@/models";
+import { useAppDispatch } from "@/utils/hooks/useAppDispatch";
+import { setLists } from "@/store/slices/Lists";
+
 import GraphSection from "./GraphSection";
+import AnalyticsSectionBar from "./AnalyticsSectionBar";
 import StatisticsSection from "./StatisticsSection";
 
 const AnalyticsSection: React.FC = () => {
+  const [data, setData] = React.useState<AnalyticsType>();
+
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    (async () => {
+      const allTasksAnalyticsPageResponse = await fetch(
+        "http://localhost:3000/tasks/analytics/all/api"
+      );
+
+      const allTasksAnalyticsPageData =
+        await allTasksAnalyticsPageResponse.json();
+
+      setData(allTasksAnalyticsPageData.data);
+      dispatch(setLists(allTasksAnalyticsPageData?.lists.data));
+
+      if (allTasksAnalyticsPageData?.accessToken) {
+        localStorage.setItem(
+          "accessToken",
+          allTasksAnalyticsPageData.accessToken
+        );
+      }
+    })();
+  }, []);
+
   return (
     <StyledAnalyticsSection>
       <AnalyticsSectionBar />
       <div className="content--analytics">
-        <GraphSection />
-        <StatisticsSection />
+        <GraphSection tasksInfo={data?.tasksAnalytics} />
+        <StatisticsSection data={data} />
       </div>
     </StyledAnalyticsSection>
   );
-}
+};
 
 const StyledAnalyticsSection = styled.section`
   width: 100%;
