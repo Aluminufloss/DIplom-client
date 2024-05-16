43647db56ab466bae0c6255106700be830c10084
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-16 23:39:36 +0300

    Update create and delete task methods in lists store (#19)

--- src/store/slices/Lists/index.ts
+++ src/store/slices/Lists/index.ts
@@ -7,7 +7,7 @@ import {
   isPendingAction,
   isRejectedAction,
 } from "@/utils/checkReduxActions";
-import { createTask, deleteTask } from "../Tasks/thunks";
+import { createTask, deleteTask, updateTask } from "../Tasks/thunks";
 
 export const listsInfo = createSlice({
   name: "listsInfo",
@@ -26,17 +26,51 @@ export const listsInfo = createSlice({
         (item) => item.listId !== action.meta.arg
       );
     });
-    builder.addCase(deleteTask.fulfilled, (state, action) => {
-      const currentList = state.lists.find(
-        (list) => list.listId === action.meta.arg.listId
-      );
+    builder
+      .addCase(createTask.fulfilled, (state, action) => {
+        const listId = action.meta.arg.listId;
 
-      if (currentList) {
-        currentList.tasks = currentList.tasks.filter(
-          (item) => item.taskId !== action.meta.arg.taskId
+        if (!listId) {
+          return;
+        }
+
+        const task = action.payload;
+
+        const currentList = state.lists.find((list) => list.listId === listId[0]);
+
+        if (currentList) {
+          currentList.tasks = [task, ...currentList.tasks];
+        }
+      })
+      .addCase(deleteTask.fulfilled, (state, action) => {
+        const currentList = state.lists.find(
+          (list) => list.listId === action.meta.arg.listId
         );
-      }
-    });
+
+        if (currentList) {
+          currentList.tasks = currentList.tasks.filter(
+            (item) => item.taskId !== action.meta.arg.taskId
+          );
+        }
+      })
+      .addCase(updateTask.fulfilled, (state, action) => {
+        if (action.meta.arg.listId?.length !== 1) {
+          return;
+        }
+
+        const currentList = state.lists.find(
+          (list) => list.listId === action.meta.arg.listId?.[0]
+        );
+
+        if (currentList) {
+          currentList.tasks = currentList.tasks.map((item) => {
+            if (item.taskId === action.meta.arg.taskId) {
+              return action.payload;
+            }
+            return item;
+          });
+        }
+      });
     builder.addCase(createTask.rejected, () => {});
     builder.addMatcher(isPendingAction, (state) => {
       state.isLoading = true;
