f76b1821bfa25a830e30d104496d8ecbe403a0c2
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-16 23:39:11 +0300

    Update group store delete, add, remove list methods (#19)

--- src/store/slices/Groups/index.ts
+++ src/store/slices/Groups/index.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 import { GroupType } from "@/models";
+import { addGroup, addListToGroup, deleteGroup, removeListFromGroup } from "./thunks";
 
 import { GroupInitialState } from "./initialState";
 import { isFulfilledAction, isPendingAction, isRejectedAction } from "@/utils/checkReduxActions";
@@ -24,6 +25,31 @@ export const groupsInfo = createSlice({
     },
   },
   extraReducers: (builder) => {
+    builder.addCase(deleteGroup.fulfilled, (state, action) => {
+      const deletedGroupId = action.meta.arg;
+      state.groups = state.groups.filter((item) => item.id !== deletedGroupId);
+    });
+    builder.addCase(addGroup.fulfilled, (state, action: PayloadAction<GroupType>) => {
+      state.groups = [action.payload, ...state.groups];
+    });
+    builder.addCase(addListToGroup.fulfilled, (state, action) => {
+      console.log('sussy', action)
+      state.groups = state.groups.map((item) => {
+        if (item.id === action.meta.arg.groupId) {
+          return { ...item, lists: [...item.lists, action.payload] };
+        }
+        return item;
+      })
+    });
+    builder.addCase(removeListFromGroup.fulfilled, (state, action) => {
+      const group = state.groups.find((item) => item.id === action.meta.arg.groupId);
+
+      if (group) {
+        group.lists = group.lists.filter((item) => item.listId !== action.meta.arg.listId);
+      }
+
+      state.groups = [...state.groups];
+    });
     builder.addCase(createTask.rejected, () => {});
     builder.addMatcher(isPendingAction, (state) => {
       state.isLoading = true;
