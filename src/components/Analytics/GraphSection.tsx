eb912e3246b919ab402778523ba873d44e1ff051
Nazar Ilchenko <nilchenko2020@gmail.com>
2024-05-17 22:26:25 +0300

    Update line chart to graph section (#19)

--- src/components/Analytics/GraphSection.tsx
+++ src/components/Analytics/GraphSection.tsx
@@ -3,47 +3,89 @@
 import styled, { useTheme } from "styled-components";
 import * as React from "react";
 
-import { GraphSectionType, TasksAnalyticsType } from "@/models";
+import {
+  GraphSectionType,
+  LinearGraphSectionType,
+  TasksAnalyticsType,
+} from "@/models";
 
 import { PieChart } from "@mui/x-charts/PieChart";
+import { LineChart } from "@mui/x-charts/LineChart";
+
 import { makeGraphSections } from "./utils/makeGraphSections";
 
 type PropsTYpe = {
   tasksInfo?: TasksAnalyticsType;
+  type: "pie" | "linear";
 };
 
 const GraphSection: React.FC<PropsTYpe> = (props) => {
   const [graphSections, setGraphSections] = React.useState<GraphSectionType[]>(
     []
   );
+  const [LinearSections, setLinearSections] =
+    React.useState<LinearGraphSectionType>([]);
+
   const theme = useTheme();
 
   React.useEffect(() => {
     if (props.tasksInfo) {
       const graphSectionsData = makeGraphSections(props.tasksInfo);
-      setGraphSections(graphSectionsData);
+
+      if (props.type === "linear") {
+        setLinearSections(graphSectionsData);
+      } else {
+        setGraphSections(graphSectionsData);
+      }
     }
   }, [props.tasksInfo]);
 
   return (
     <StyledGraphSection>
-      <PieChart
-        series={[
-          {
-            data: graphSections,
-            highlightScope: { faded: "global", highlighted: "item" },
-            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
-          },
-        ]}
-        slotProps={{
-          legend: { hidden: true },
-        }}
-        colors={[theme.colorValues.green, theme.colorValues.primary, theme.colorValues.error]}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        width={700}
-        height={450}
-        className="graph"
-      />
+      {props.type === "pie" ? (
+        <PieChart
+          series={[
+            {
+              data: graphSections,
+              highlightScope: { faded: "global", highlighted: "item" },
+              faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+            },
+          ]}
+          slotProps={{
+            legend: { hidden: true },
+          }}
+          colors={[
+            theme.colorValues.green,
+            theme.colorValues.primary,
+            theme.colorValues.error,
+          ]}
+          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          width={700}
+          height={450}
+          className="graph"
+        />
+      ) : (
+        <LineChart
+          series={[
+            {
+              data: linearData,
+              highlightScope: { faded: "global", highlighted: "item" },
+            },
+          ]}
+          slotProps={{
+            legend: { hidden: true },
+          }}
+          colors={[
+            theme.colorValues.green,
+            theme.colorValues.primary,
+            theme.colorValues.error,
+          ]}
+          margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          width={700}
+          height={450}
+          className="graph"
+        />
+      )}
     </StyledGraphSection>
   );
 };
